a97b0f31b501603aa5a36b4cd83843a9f8914fad
taiki-jp <senjuu444@gmail.com>
2022-05-21 20:19:59 +0900

    first commit

---
+++ members/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('add/add_record/', views.add_record, name='add_record'),
+]
---
+++ members/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Members
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def add_record(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
