ed508a78fc40f617c8f83f6a8d47321d3ba1304d
taiki-jp <senjuu444@gmail.com>
2022-05-22 18:23:26 +0900

    :sparkles: w3schools tutorial

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+# メソッドとURLパターンの紐づけ
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
     path('add/add_record/', views.add_record, name='add_record'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/update_record/<int:id>', views.update_record, name='update_record'),
 ]
